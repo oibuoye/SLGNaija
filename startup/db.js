9823dfd6448f7497be6bf3c0319c0aca0c7efbfd
oibuoye <oibuoye@parkwayprojects.com>
2019-06-04 17:38:55 +0100

    Added mongoose.set('useCreateIndex', true) to the db connection

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,7 @@ const config = require('config');
 
 module.exports = function(){
   const db = config.get('db');
-    //mongoose.set('useCreateIndex', true);
+    mongoose.set('useCreateIndex', true);
     mongoose.connect(db, { useNewUrlParser: true })
     .then(() => winston.info(`Connected to ${db}....`));
 }
