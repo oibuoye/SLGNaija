7ca676e3203262525a7606a28f61680474cf28ae
opecodeforliving <ope4sure1@gmail.com>
2019-06-15 18:43:16 +0100

    Added api endpoint to get local governments by each state. Also, prevent insertion of duplicate local government

--- routes/lgas.js
+++ routes/lgas.js
@@ -5,7 +5,7 @@ const { State } = require('../models/state');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const lgas = await LGA.find();
+  const lgas = await LGA.find().select('-__v');
   res.send(lgas);
 });
 
@@ -14,9 +14,15 @@ router.post('/', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   //Check if the state in the payload exist in the db
-  const isStateExist = await State.findOne({ stateName: req.body.stateName });
-  if (isStateExist == null) {
+  const isStateExist = await State.findOne({ name: req.body.stateName });
+  if (isStateExist === null) {
     return res.status(400).send("State doesn't exist");
+  }
+
+  //Check if the lga in the payload already exist in the db
+  const isLGAExist = await LGA.findOne({ stateName: req.body.stateName, LGAName: req.body.LGAName });
+  if (isLGAExist !== null) {
+    return res.status(400).send("LGA name already exist");
   }
 
   let lga = new LGA({ LGAName: req.body.LGAName, stateName: req.body.stateName });
@@ -47,16 +53,24 @@ router.delete('/:id', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  const lga = await LGA.findById(req.params.id);
+  const lga = await LGA.findById(req.params.id).select('-__v');
 
   if (!lga) return res.status(404).send('The LGA with the given ID was not found. ');
 
   res.send(lga);
 });
 
+router.post('/getbystatename', async (req, res) => {
+  const lga = await LGA.find({ stateName: req.body.stateName }).select('-__v');
+  if (lga.length === 0) return res.status(404).send('The LGA with the state name ' + req.body.stateName + ' was not found. ');
+
+  res.send(lga);
+});
+
+
 router.post('/bundle', async (req, res) => {
-  const requestPayLoad = req.body; 
-  if(requestPayLoad.length === 0){
+  const requestPayLoad = req.body;
+  if (requestPayLoad.length === 0) {
     return res.status(400).send("Bad request");
   }
 
@@ -65,12 +79,18 @@ router.post('/bundle', async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
 
     const isStateExist = await State.findOne({ name: requestPayLoad[i].stateName });
-    if (isStateExist == null) {
+    if (isStateExist === null) {
       return res.status(400).send("State doesn't exist. State Name => " + requestPayLoad[i].stateName);
     }
 
-    let lga = new LGA({ LGAName: requestPayLoad[i].LGAName, stateName: requestPayLoad[i].stateName });
-    lga = await lga.save();
+    //Check if the lga in the payload already exist in the db
+    //Skip if exist, and insert if not
+    const isLGAExist = await LGA.findOne({ stateName: requestPayLoad[i].stateName, LGAName: requestPayLoad[i].LGAName });
+    if (isLGAExist === null) {
+      let lga = new LGA({ LGAName: requestPayLoad[i].LGAName, stateName: requestPayLoad[i].stateName });
+      lga = await lga.save();
+    }
+
   }
 
   res.send("Created successfully");
